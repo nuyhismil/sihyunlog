df9c2538952f0b5c07d606dc2f9d34a97cc38fd2
임시현 <gibbmi0528@gmail.com>
2025-07-21 03:49:18 +0900

    super super minor changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     return (
       <footer className="py-6 mt-12">
         <div className="container mx-auto text-center text-sm text-gray-500 px-4">
-          don't let your friends find out this blog.
+          do not let your friends find out this blog.
         </div>
       </footer>
     );
